8e5a910d4792f42d7be4e710e4f8833cf9ce13d3
Ali-Sultan19 <alisultanmr@gmail.com>
2025-04-28 20:47:18 +0500

    Add files via upload

---
+++ Case Statement.py
@@ -0,0 +1,20 @@
+a = int(input("Please enter a Roll Number : "))
+match a:
+    case 1:
+        print("Your Name is Ali")
+    case 2:
+        print("Your Name is Manahil")
+    case 3:
+        print("Your Name is Numan")
+    case 4:
+        print("Your Name is Farzam")
+    case 5:
+        print("Your Name is Rabi")
+    case 6:
+        print("Your Name is Komal")
+    case 7:
+        print("Your Name is Anaya")
+    case 8:
+        print("Your Name is lawiza")
+    case 9:
+        print("Your Name is Rahim")
