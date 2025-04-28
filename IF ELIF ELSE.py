1f396e4ce937c0825ceded2cf4ff3511117b5dd3
Ali-Sultan19 <alisultanmr@gmail.com>
2025-04-28 20:30:43 +0500

    Add files via upload

---
+++ IF ELIF ELSE.py
@@ -0,0 +1,17 @@
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+# Conditional operators
+# >, <, >=, <=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>18):
+  print("You can drive")
+  print("Yes")
+elif(a==18):
+  print("You cannot drive")
+  print("No")
+  print("Yay!")
+else:
+  print("You are too young to drive")
---
+++ good morning.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a time: "))
+print("The time is :", a)
+if (a <= 8):
+    print("Good Morning")
+elif (a <= 16):
+    print("Good Afternoon")
+elif (a <= 24):
+    print("Good Evening")
+else:
+    print("Good Night")
+
