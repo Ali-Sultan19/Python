7d905966cde3dcb29477dd567fcf449a3ee8f4f1
Ali-Sultan19 <alisultanmr@gmail.com>
2025-04-28 21:02:37 +0500

    Add files via upload

---
+++ func1.py
@@ -0,0 +1,11 @@
+def info(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+for i in  range(1,3):
+    a = input("Enter your name : ")
+    b = input("Enter your age : ")
+    c = input("Enter your roll no : ")
+    info(a, b, c)
+
+
